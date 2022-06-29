7c09f3e1c1ff34ec35795997f95b4901c018f230
Ramiro Peña <93239263+ramiropen@users.noreply.github.com>
2022-06-28 19:37:34 -0700

    mkdir petfax

---
+++ app.py
@@ -0,0 +1,21 @@
+from petfax import create_app
+app = create_app()
+
+# # config                    
+# from flask import Flask
+# app = Flask(__name__)
+
+# # index route
+# @app.route('/')
+# def index(): 
+#     return 'Hello, this is PetFax!'
+
+# # pets index route
+# @app.route('/pets')
+# def pets(): 
+#     return 'These are our pets available for adoption!'
+
+# # instead of flask run, you can use this
+# if __name__ == "__main__":
+#     app.run()
+
--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,14 +1,25 @@
-# import Flask packages
-from flask import Flask
+from flask import Flask 
 
-# Define function (create_app)
-def create_app():
-    # new app instance fo Flask.
+def create_app(): 
     app = Flask(__name__)
-    
-    # basic index route goes to ('/'), and returns 'Hello' comment.
+
     @app.route('/')
-    def hello():
-        return ('Hello, PetFax!')
+    def hello(): 
+        return 'Hello, PetFax!'
 
     return app
+
+# # import Flask packages
+# from flask import Flask
+
+# # Define function (create_app)
+# def create_app():
+#     # new app instance fo Flask.
+#     app = Flask(__name__)
+    
+#     # basic index route goes to ('/'), and returns 'Hello' comment.
+#     @app.route('/')
+#     def hello():
+#         return ('Hello, PetFax!')
+
+#     return app
