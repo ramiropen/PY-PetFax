423cc314a77a0c894e7bbf468915527586858f4a
Ramiro Peña <93239263+ramiropen@users.noreply.github.com>
2022-06-30 20:13:19 -0700

    added database, added more folders, etc

--- petfax/pet.py
+++ petfax/pet.py
@@ -1,10 +1,4 @@
-# Create a new instance of Blueprint and save it to a variable called bp.
-# Remember, creating a new blueprint instance requires three arguments:
-# The name of the blueprint. Let's name ours pet.
-# The location of the blueprint. We can just use the handy __name__.
-# The URL prefix that should be used for all 
-# routes attached to this blueprint. Let's go with /pets.
-from flask import ( Blueprint, render_template ) 
+from flask import ( Blueprint, render_template, models ) 
 import json 
 
 pets = json.load(open('pets.json'))
@@ -19,6 +13,23 @@ def index():
 def show(id): 
     pet = pets[id - 1]
     return render_template('pets/show.html', pet=pet)
+
+@bp.route('/<int:id>')
+def show(id): 
+    user = models.User.query.filter_by(id=id).first()
+    user_dict = {
+        'username': user.username
+    }
+
+    return user_dict
+
+# ---------------------------------------------------------------------------
+# Create a new instance of Blueprint and save it to a variable called bp.
+# Remember, creating a new blueprint instance requires three arguments:
+# The name of the blueprint. Let's name ours pet.
+# The location of the blueprint. We can just use the handy __name__.
+# The URL prefix that should be used for all 
+# routes attached to this blueprint. Let's go with /pets.
 # -------------------------------------------------------
 # from flask import ( Blueprint, render_template ) 
 # import json
